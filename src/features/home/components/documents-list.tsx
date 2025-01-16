c5d511098ba7683d67e9f81557280a8de3628f8c
Mohammad <bhshtbaran@gmail.com>
2025-01-16 08:08:16 -0800

    feat: handle no documents

--- src/features/home/components/documents-list.tsx
+++ src/features/home/components/documents-list.tsx
@@ -11,41 +11,45 @@ const DocumentsList = async () => {
 
   return (
     <ul className="w-full flex flex-col gap-y-5 mt-10">
-      {documents?.data.map(({ id, createdAt, metadata: { title } }) => {
-        const formattedDate = dateFormatter(createdAt.toString());
+      {(documents?.data.length ?? 0) > 0 ? (
+        <p className="text-lg text-center text-muted">No documents found</p>
+      ) : (
+        documents?.data.map(({ id, createdAt, metadata: { title } }) => {
+          const formattedDate = dateFormatter(createdAt.toString());
 
-        return (
-          <li
-            key={id}
-            className="w-full rounded-md shadow-xl p-5 bg-[url('/background-image.png')] bg-cover"
-          >
-            <Link
-              href={`/documents/${id}`}
-              className="w-full flex items-center gap-x-4"
+          return (
+            <li
+              key={id}
+              className="w-full rounded-md shadow-xl p-5 bg-[url('/background-image.png')] bg-cover"
             >
-              <div className="hidden rounded-md p-2 bg-dark-500 sm:block">
-                <Image
-                  src="/document.svg"
-                  alt="Document"
-                  width={40}
-                  height={40}
-                />
-              </div>
-              <div className="w-full flex justify-between items-center">
-                <div className="space-y-1">
-                  <span className="text-lg line-clamp-1">{title}</span>
-                  <span className="text-sm font-light text-blue-100">
-                    {formattedDate}
-                  </span>
+              <Link
+                href={`/documents/${id}`}
+                className="w-full flex items-center gap-x-4"
+              >
+                <div className="hidden rounded-md p-2 bg-dark-500 sm:block">
+                  <Image
+                    src="/document.svg"
+                    alt="Document"
+                    width={40}
+                    height={40}
+                  />
                 </div>
-                <div>
-                  <DeleteDocumentModal documentId={id} />
+                <div className="w-full flex justify-between items-center">
+                  <div className="space-y-1">
+                    <span className="text-lg line-clamp-1">{title}</span>
+                    <span className="text-sm font-light text-blue-100">
+                      {formattedDate}
+                    </span>
+                  </div>
+                  <div>
+                    <DeleteDocumentModal documentId={id} />
+                  </div>
                 </div>
-              </div>
-            </Link>
-          </li>
-        );
-      })}
+              </Link>
+            </li>
+          );
+        })
+      )}
     </ul>
   );
 };
